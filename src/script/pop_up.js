05f73ae7d858334f25f5c49a14d337d18d1d49ea
berto929 <davidabertho.94@gmail.com>
2024-07-15 16:37:37 +0100

    admin dashboard done

---
+++ src/script/pop_up.js
@@ -0,0 +1,24 @@
+class Pop_up {
+  constructor(pop_up_window, open_btn, close_btn) {
+    this.main = document.querySelector(pop_up_window);
+    this.openBtn = document.querySelector(open_btn);
+    this.close_btn = document.querySelector(close_btn);
+
+    // AJout des ecouters des evenement
+    this.openBtn.addEventListener("click", this.open);
+    this.close_btn.addEventListener("click", this.close);
+  }
+
+  set_display = (_display) => {
+    this.main.style.display = _display;
+  };
+  open = () => {
+    this.set_display("grid");
+  };
+  close = () => {
+    console.log(this.main);
+    this.set_display("none");
+  };
+}
+
+test = new Pop_up(".pop-up-bg", ".add-btn", ".close-btn");
